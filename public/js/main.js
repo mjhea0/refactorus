cd66b0efef50c8c85abe443cef17a569d6092c8b
Michael Herman <hermanmu@gmail.com>
2013-11-25 19:52:11 -0700

    initial

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   var imageUrl = $('body').find('img').attr('src');
   var imgTag = ["<img src='", imageUrl, "'>"].join('');
-  for (var i=1;i<40;i++) {$('#images').append(imgTag);}
+  for (var i=1;i<1;i++) {$('#images').append(imgTag);}
 });
